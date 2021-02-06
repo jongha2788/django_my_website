7251d26d31e51be137eb6fc81e22f14819e6e58f
Jongha <jongha2788@gmail.com>
2021-02-06 23:49:30 +0900

    blog app -Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# user 라는 객체 이미 제공됨
+ # 사용자 james 가 삭제 되었을 때 글 쓴 거 삭제 해줌
